3598b5cb95e0e4d90280e1b1894584c5f5e7da5c
Ian Worley <iworley0001@student.fortosage.net>
2018-03-02 19:54:19 +0000

    modified:   script.py

--- script.py
+++ script.py
@@ -6,14 +6,15 @@ def hm():
     for i in range(5):
         output=''
         guess += raw_input('what is your guess ')
-    for letter in word:
-        if letter ==' ':
-            output += ' '
-            continue
-        if letter in guess:
-            output+=letter
-        else: 
-            output += '-'
-    print (output)
+        for letter in word:
+            if letter ==' ':
+                output += ' '
+                continue
+            if letter in guess:
+                output+=letter
+            else: 
+                output += '-'
+        print (output)
     if output == word:
         print ('finsh')
+hm()
